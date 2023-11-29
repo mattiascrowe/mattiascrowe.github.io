ae62653f29b41bc8a7dec07e27b5b253def641d0
mattiascrowe <150852255+mattiascrowe@users.noreply.github.com>
2023-11-29 15:50:16 +0000

    Java test 1
    small JS test

---
+++ vvs.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.sidenav') .sidenav({
+                edge:'right',
+                preventscrolling:false,
+    });
+
+    $('.carousel').carousel();
+    $('.modal').modal();
+    $('.collapsible').collapsible();
+});
+
